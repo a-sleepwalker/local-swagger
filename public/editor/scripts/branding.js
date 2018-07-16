e28967cf5a3c989e5bb38cb6d888ac6c71aba881
taowt <msuserting@hotmail.com>
2018-07-16 19:12:57 +0800

    upd

---
+++ public/editor/scripts/branding.js
@@ -0,0 +1,12 @@
+/*
+ * branding.js can be used for overloading Swagger Editor behaviors
+ * You can add new controllers to Swagger Editor with following syntax:
+ *
+ * SwaggerEditor.controller('BrandingController', function($scope) { ... });
+ *
+ * You can use the controller you created in branding HTML pieces.
+*/
+
+SwaggerEditor.controller('BrandingController', function ($scope) {
+    console.log($scope);
+});
---
+++ routes/index.js
@@ -0,0 +1,52 @@
+const router = require('koa-router')();
+const fs = require('fs');
+const path = require('path');
+const promisify = require('util').promisify;
+const readFileAsync = promisify(fs.readFile);
+const CONFIG_PATH = path.join(__dirname, '../public/conf/conf.yml');
+
+router.get('/editor_config', async (ctx, next) => {
+    try {
+        ctx.set('content-type', 'application/yaml');
+        ctx.response.body = await readFileAsync(CONFIG_PATH, {
+            encode: 'utf-8',
+            flag: 'r'
+        });
+    } catch (e) {
+        ctx.response.body = {
+            code: 'app-50x',
+            message: e.message
+        };
+    }
+});
+
+router.put('/editor_config', async (ctx, next) => {
+    if (ctx.is('application/*') && ctx.accepts('application/*')) {
+        let result = await new Promise((resolve, reject) => {
+            try {
+                let ws = fs.createWriteStream(CONFIG_PATH);
+                ctx.req.pipe(ws);
+                resolve('finished');
+            } catch (e) {
+                reject(e.message)
+            }
+        });
+        if (result === 'finished') {
+            ctx.body = {
+                message: 'done',
+                code: 'app-200'
+            };
+        } else {
+            ctx.body = {
+                message: result,
+                code: 'app-500'
+            };
+        }
+    } else {
+        ctx.body = {
+            message: 'invalid content type',
+            code: 'app-505'
+        };
+    }
+});
+module.exports = router;
